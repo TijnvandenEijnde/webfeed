31274ee3ec06279f07771a0fe6b9e9671193bfe0
Tijn <tijnvdeijnde@outlook.com>
2024-02-11 19:35:06 +0100

    Optimized code to reuse the same function

--- lib/util/datetime.dart
+++ lib/util/datetime.dart
@@ -4,18 +4,19 @@ const rfc822DatePattern = 'EEE, dd MMM yyyy HH:mm:ss Z';
 const rfc822DatePatternWithoutSeconds = 'EEE, dd MMM yyyy HH:mm Z';
 const rfc822DatePatternWithoutDayOfWeek = 'dd MMM yyyy HH:mm:ss Z';
 
-DateTime? parseDateTime(dateString) {
+DateTime? parseDateTime(String? dateString) {
   if (dateString == null) return null;
-  return _parseRfc822DateTime(dateString) ??
-      _parseRfc822DateTimeWithoutSeconds(dateString) ??
-      _parseRfc822DateTimeWithoutDayOfWeek(dateString) ??
+
+  return _tryParseRfc822Date(dateString, rfc822DatePattern) ??
+      _tryParseRfc822Date(dateString, rfc822DatePatternWithoutSeconds) ??
+      _tryParseRfc822Date(dateString, rfc822DatePatternWithoutDayOfWeek) ??
       _parseIso8601DateTime(dateString);
 }
 
-DateTime? _parseRfc822DateTime(String dateString) {
+DateTime? _tryParseRfc822Date(String dateString, String pattern) {
   try {
-    final num? length = dateString.length.clamp(0, rfc822DatePattern.length);
-    final trimmedPattern = rfc822DatePattern.substring(0, length as int?); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
+    final num? length = dateString.length.clamp(0, pattern.length);
+    final trimmedPattern = pattern.substring(0, length as int);
     final format = DateFormat(trimmedPattern, 'en_US');
     return format.parse(dateString);
   } on FormatException {
@@ -23,29 +24,7 @@ DateTime? _parseRfc822DateTime(String dateString) {
   }
 }
 
-DateTime? _parseRfc822DateTimeWithoutSeconds(String dateString) {
-  try {
-    final num? length = dateString.length.clamp(0, rfc822DatePatternWithoutSeconds.length,);
-    final trimmedPattern = rfc822DatePatternWithoutSeconds.substring(0, length as int?); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
-    final format = DateFormat(trimmedPattern, 'en_US');
-    return format.parse(dateString);
-  } on FormatException {
-    return null;
-  }
-}
-
-DateTime? _parseRfc822DateTimeWithoutDayOfWeek(String dateString) {
-  try {
-    final num? length = dateString.length.clamp(0, rfc822DatePatternWithoutDayOfWeek.length,);
-    final trimmedPattern = rfc822DatePatternWithoutDayOfWeek.substring(0, length as int?);
-    final format = DateFormat(trimmedPattern, 'en_US');
-    return format.parse(dateString);
-  } on FormatException {
-    return null;
-  }
-}
-
-DateTime? _parseIso8601DateTime(dateString) {
+DateTime? _parseIso8601DateTime(String dateString) {
   try {
     return DateTime.parse(dateString);
   } on FormatException {
